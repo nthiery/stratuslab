2374e311ee4a8c17415046004b42c2a40051cd6e
Nicolas M. Thiéry <nthiery@users.sf.net>
2014-05-02 10:47:48 +0200

    Added two scripts for Sage virtual machines

---
+++ VirtualMachines/initSage.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Run the VM with:
+# stratus-run-instance -type=m1.small --volatile-disk 20 --cloud-init 'ssh,$HOME/.ssh/id_rsa.pub#none,./initSage.sh' BsHnKXtlxejHFYIq1oTQvFE2sZy
+# update the system
+apt-get update
+apt-get dist-upgrade -y
+# add the following packages:
+# -libfontconfig or SAGE will crash
+# -git, libssl, make, m4 to allow SAGE to upgrade (neutralized for now)
+apt-get install libfontconfig1 -y
+# apt-get install git libssl0.9.8 make m4 dpkg-dev -y
+
+# Add the volatile disk. On this VM, its mount point is /dev/sdc
+yes | mkfs.ext4 /dev/sdc
+mkdir /mnt/volatile
+mount /dev/sdc /mnt/volatile
+
+# download the current version of SAGE for ubuntu and untar it in the volatile folder
+cd /mnt/volatile
+wget http://www-ftp.lip6.fr/pub/math/sagemath/linux/64bit/sage-6.1.1-x86_64-Linux-Ubuntu_8.04_x86_64.tar.gz
+tar zxf sage-6.1.1-x86_64-Linux-Ubuntu_8.04_x86_64.tar.gz
+rm sage-6.1.1-x86_64-Linux-Ubuntu_8.04_x86_64.tar.gz
+cd sage-6.1.1-x86_64-Linux/
+
+# set path to SAGE binaries
+ln -s /mnt/volatile/sage-6.1.1-x86_64-Linux/sage /usr/local/bin/sage
+# upgrade SAGE. If all the libs are not there, it will only update SAGE paths, which is already very good.
+sage -upgrade
+
+# Done!
---
+++ VirtualMachines/sage-patchbot.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Run the VM with:
+# stratus-run-instance --cpu=8 --ram=4000 --swap==2000 --volatile-disk 20 --cloud-init 'ssh,$HOME/.ssh/id_rsa.pub#none,./initSage.sh' BsHnKXtlxejHFYIq1oTQvFE2sZy
+# stratus-run-instance --cpu=8 --ram=4000 --swap==2000 --volatile-disk 20 BsHnKXtlxejHFYIq1oTQvFE2sZy
+# stratus-connect-instance 2721
+# update the system
+apt-get update
+apt-get dist-upgrade -y
+# add the following packages:
+# -libfontconfig or SAGE will crash
+# -libcurl4-openssl-dev required by sage's git; otherwise it does not recognise the https protocol
+#  this might not be enough
+# Using the atlas libraries from the distribution would save much compilation time; on 13.10, the package is named libatlas3-base
+# I tried with libatlas3gf-base on 12.10, but it failed so far.
+apt-get install libfontconfig1 libssl-dev build-essential m4 libcurl4-openssl-dev git -y
+
+reboot
+
+# Add the volatile disk. On this VM, its mount point is /dev/sdc
+# TODO: mount automatically /mnt/volatile at each boot
+yes | mkfs.ext4 /dev/sdc
+mkdir /mnt/volatile
+mount /dev/sdc /mnt/volatile
+
+# Create sage user
+# TODO: execute the rest of this script as Sage
+useradd -p xxx -m -d /mnt/volatile/sage -s /bin/bash sage
+su - sage
+
+git clone git://github.com/sagemath/sage.git
+cd sage
+#export SAGE_ATLAS_LIB=/usr/lib/
+export SAGE_ATLAS_ARCH=base
+export MAKE="make -j8"
+make
+
+cat > patchbotconfig<<EOF
+{
+  "use_ccache": false
+}
+EOF
+./sage -patchbot --ticket 15801 --config patchbotconfig 2>&1 | tee log
+./sage -patchbot
+
+# set path to SAGE binaries
+#ln -s /mnt/volatile/sage-6.1.1-x86_64-Linux/sage /usr/local/bin/sage
+
+# Done!
