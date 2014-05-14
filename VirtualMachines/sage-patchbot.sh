b34fef56b2251b3a3bc4b5d9983b67c5b215d509
Nicolas M. Thiéry <nthiery@users.sf.net>
2014-05-14 08:48:13 +0200

    Added run/kill/shell to the stratuslab frontend script

--- VirtualMachines/sage-patchbot.sh
+++ VirtualMachines/sage-patchbot.sh
@@ -5,19 +5,19 @@
 # Run the VM with:
 # stratus-run-instance --cpu=8 --ram=4000 --swap==2000 --volatile-disk 20 --cloud-init 'ssh,$HOME/.ssh/id_rsa.pub#none,./initSage.sh' BsHnKXtlxejHFYIq1oTQvFE2sZy
 # stratus-run-instance --cpu=8 --ram=4000 --swap==2000 --volatile-disk 20 BsHnKXtlxejHFYIq1oTQvFE2sZy
+# stratus-run-instance --vm-name sage-patchbot --cpu=8 --ram=4000 --swap==2000 --volatile-disk 20 KhGzWhB9ZZv5ZkLSZqm6pkWx7ZF #Ubuntu 14.04
 # stratus-connect-instance 2721
 # update the system
 apt-get update
-apt-get dist-upgrade -y
+apt-get dist-upgrade -y             # could we have an already dist-upgraded Ubuntu 14.04?
 # add the following packages:
-# -libfontconfig or SAGE will crash
-# -libcurl4-openssl-dev required by sage's git; otherwise it does not recognise the https protocol
+# -libfontconfig: or SAGE will crash
+# -libcurl4-openssl-dev: required by sage's git; otherwise it does not recognise the https protocol
 #  this might not be enough
 # Using the atlas libraries from the distribution would save much compilation time; on 13.10, the package is named libatlas3-base
 # I tried with libatlas3gf-base on 12.10, but it failed so far.
-apt-get install libfontconfig1 libssl-dev build-essential m4 libcurl4-openssl-dev git -y
-
-reboot
+# texlive: we could reduce to a subset
+apt-get install libfontconfig1 libssl-dev build-essential m4 libcurl4-openssl-dev git libatlas3-base texlive-latex-recommended texlive-science texlive-lang-all
 
 # Add the volatile disk. On this VM, its mount point is /dev/sdc
 # TODO: mount automatically /mnt/volatile at each boot
@@ -28,11 +28,19 @@ mount /dev/sdc /mnt/volatile
 # Create sage user
 # TODO: execute the rest of this script as Sage
 useradd -p xxx -m -d /mnt/volatile/sage -s /bin/bash sage
-su - sage
+mkdir ~sage/.ssh
+chmod 700 ~sage/.ssh
+cp .ssh/authorized_keys ~sage/.ssh/
+chown -R sage:sage ~sage/.ssh
+
+reboot
+
+ssh sage@...
+screen
 
 git clone git://github.com/sagemath/sage.git
 cd sage
-#export SAGE_ATLAS_LIB=/usr/lib/
+export SAGE_ATLAS_LIB=/usr/lib/
 export SAGE_ATLAS_ARCH=base
 export MAKE="make -j8"
 make
@@ -42,6 +50,7 @@ cat > patchbotconfig<<EOF
   "use_ccache": false
 }
 EOF
+./sage -i patchbot
 ./sage -patchbot --ticket 15801 --config patchbotconfig 2>&1 | tee log
 ./sage -patchbot
 
