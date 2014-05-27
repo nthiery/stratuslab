54e5334c4d93d1db634da96244759f3b6258288e
Nicolas M. Thiéry <nthiery@users.sf.net>
2014-05-27 17:40:49 +0200

    sage-patchbot.sh: toward a more automatic build script

--- VirtualMachines/sage-patchbot.sh
+++ VirtualMachines/sage-patchbot.sh
@@ -15,45 +15,55 @@ apt-get dist-upgrade -y             # could we have an already dist-upgraded Ubu
 # -libcurl4-openssl-dev: required by sage's git; otherwise it does not recognise the https protocol
 #  this might not be enough
 # Using the atlas libraries from the distribution would save much compilation time; on 13.10, the package is named libatlas3-base
-# I tried with libatlas3gf-base on 12.10, but it failed so far.
 # texlive: that's for compiling doc-pdfwe could reduce to a subset
 # texinfo: for scipy?
-apt-get install libfontconfig1 libssl-dev build-essential m4 libcurl4-openssl-dev git libatlas3-base texlive-latex-recommended texlive-science texlive-lang-all texinfo
+apt-get install -y libfontconfig1 libssl-dev build-essential m4 libcurl4-openssl-dev git libatlas3-base texlive-latex-recommended texlive-science texlive-lang-all texinfo
 
 # Add the volatile disk. On this VM, its mount point is /dev/sdc
 # TODO: mount automatically /mnt/volatile at each boot
 yes | mkfs.ext4 /dev/sdc
 mkdir /mnt/volatile
-mount /dev/sdc /mnt/volatile
+cat >> /etc/fstab <<EOF
+/dev/sdc	/mnt/volatile	ext4	errors=remount-ro	0	1
+EOF
+mount -a
 
-# Create sage user
-# TODO: execute the rest of this script as Sage
+# Create Sage user and prepare its directory
 useradd -p xxx -m -d /mnt/volatile/sage -s /bin/bash sage
 mkdir ~sage/.ssh
 chmod 700 ~sage/.ssh
 cp .ssh/authorized_keys ~sage/.ssh/
-chown -R sage:sage ~sage/.ssh
 
-reboot
+cat > ~sage/start-patchbot <<EOF
+#!/bin/sh
 
-ssh sage@...
-screen
-
-git clone git://github.com/sagemath/sage.git
-cd sage
 export SAGE_ATLAS_LIB=/usr/lib/
 export SAGE_ATLAS_ARCH=base
 export MAKE="make -j8"
-make
 
-cat > patchbotconfig<<EOF
-{
-  "use_ccache": false
-}
+if [ ! -d sage ] then
+    git clone git://github.com/sagemath/sage.git
+    cd sage
+    make
+
+    #cat > patchbotconfig<<EOF
+    #{
+    #  "use_ccache": false
+    #}
+    #EOF
+else
+    cd sage
+fi
+#./sage -i patchbot
+#./sage -patchbot --ticket 15801 --config patchbotconfig 2>&1 | tee log
+#./sage -patchbot
 EOF
-./sage -i patchbot
-./sage -patchbot --ticket 15801 --config patchbotconfig 2>&1 | tee log
-./sage -patchbot
+chmod 700 ~sage/start-patchbot
+
+chown -R sage:sage ~sage
+
+reboot
+
 
 # set path to SAGE binaries
 #ln -s /mnt/volatile/sage-6.1.1-x86_64-Linux/sage /usr/local/bin/sage
